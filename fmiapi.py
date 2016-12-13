1dffd71938ab1575aa9c0636b0e5e1b2f5219942
Janne Ruotsalainen <jaruotsa@gmail.com>
2016-12-13 21:43:03 +0200

    Updated documentation.

--- fmiapi.py
+++ fmiapi.py
@@ -21,6 +21,64 @@ def getresponse(url, params):
 def fmi_observations_weather_simple(api_key, params):
     """
     Get realtime information from finnish weather stations.
+
+    From API documentation:
+    
+    Real time weather observations from weather stations. Default set contains
+    wind speed, direction, gust, temperature, relative humidity, dew point,
+    pressure reduced to sea level, one hour precipitation amount, visibility
+    and cloud cover. By default, the data is returned from last 12 hour. At
+    least one location parameter (geoid/place/fmisid/wmo/bbox) has to be given.
+    The data is returned as a simple feature format.
+
+    Possible query parameters:
+    
+    starttime       Parameter begin specifies the begin of time interval in
+                    ISO-format (for example 2012-02-27T00:00:00Z).
+
+    endtime         End of time interval in ISO-format
+                    (for example 2012-02-27T00:00:00Z).
+
+    timestep        The time step of data in minutes. Notice that timestep is
+                    calculated from start of the ongoing hour or day.
+
+    parameters      Comma separated list of meteorological parameters to
+                    return.
+    
+    crs             Coordinate projection to use in results.  For example
+                    EPSG::3067
+
+    bbox            Bounding box of area for which to return data
+                    (lon,lat,lon,lat). For example 21,61,22,62
+
+    place           The location for which to provide data. Region can be given
+                    after location name separated by comma (for example 
+                    Kumpula,Kolari).
+    
+    fmisid          Identifier of the observation station.
+
+    maxlocations    How many observation stations are fetched around queried
+                    locations. Note that stations are only searched with 50
+                    kilometers radius around the location.
+    
+    geoid           Geoid of the location for which to return data. (ID from
+                    geonames.org)
+
+    wmo             WMO code of the location for which to return data.
+
+    Returned data:
+
+    p_sea           Air pressure MSL        (hPa)
+    r_1h            Precipitation amount    (mm)
+    rh              Relative humidity       (%)
+    ri_10min        Precipitation intensity (mm/h)
+    snow_aws        Snow depth              (mm)
+    t2m             Air temperature         (deg c)
+    td              Dew-point temperature   (deg c)
+    vis             Horizontal visibility   (m)
+    wd_10min        Wind direction          (deg)
+    wg_10min        Wind gust speed         (m/s) 
+    ws_10min        Wind speed              (m/s)
     """
     url = get_fmi_url(api_key)
     response = getresponse(url, params)
